04e1906e13c2ba86dc265f2d3388d58e7afdbe98
brijeshkuntal <brijesh.kuntal@nagarro.com>
2022-06-03 14:07:55 +0530

    setting and view file changes

--- app_layer/views.py
+++ app_layer/views.py
@@ -1,11 +1,14 @@
 from .models.employee_models import Employee
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+import json
 
 @api_view(['GET'])
 def get_users(request):
     tenant = request.tenant
     print(tenant.name)
+    print(dir(request))
     emp_obj = Employee.objects.filter(empOrg=tenant.name)
     emp_list = []
     for obj in emp_obj:
@@ -16,4 +19,4 @@ def get_users(request):
             "empID": obj.empID
         }
         emp_list.append(data)
-    return Response({"data": emp_list})
+    return Response({"data": emp_list},status=status.HTTP_200_OK)
